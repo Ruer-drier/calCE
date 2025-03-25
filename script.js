7343331bd057a0ca035eec3af1fd52d1baa73ada
Ruer-drier <ruerdrier@gmail.com>
2025-03-25 09:05:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+}
+function calculate(){
+    try{
+       display.value = eval(display.value) 
+    } catch(error) {
+        display.value="Syntax error";
+    }
+}
